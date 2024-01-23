8bb739ff961749789e6aa402cd6f81d9db33690a
Heidi Sokolovski <heidi.sokolovski@gmail.com>
2024-01-23 02:41:15 +0100

    changed data json and created board

---
+++ board.py
@@ -0,0 +1,82 @@
+import json
+
+
+class City:
+    def __init__(self, name):
+        self.name = name
+        self.railways = []
+
+    def __str__(self):
+        city_str = self.name + "\n"
+        for railway in self.railways:
+            city_str += "  " + str(railway) + "\n"
+        return city_str
+
+    def add_railway(self, railway):
+        self.railways.append(railway)
+
+    def get_railway_length(self, destination):
+        railway = self.get_railway(destination)
+        return railway.length if railway else None
+
+    def get_railway_color(self, destination):
+        railway = self.get_railway(destination)
+        return railway.color if railway else None
+
+    def get_railway(self, destination):
+        temp = [self.name, destination]
+        temp.sort()
+
+        for railway in self.railways:
+            if railway.city1 == temp[0] and railway.city2 == temp[1]:
+                return railway
+
+        return None
+
+
+class Railway:
+    def __init__(self, city1, city2, length, color):
+        self.city1 = city1
+        self.city2 = city2
+        self.length = length
+        self.color = color
+
+    def __str__(self):
+        return self.city1.name + " - " + self.city2.name + " (" + str(self.length) + ", " + self.color + ")"
+
+
+class Board:
+    def __init__(self, filename):
+        self.board = {}
+        self.__make(filename)
+
+    def __str__(self):
+        board_str = ""
+        for city in self.board:
+            board_str += str(self.board[city]) + "\n"
+        return board_str
+
+    def __make(self, filename):
+        with open(filename, "r") as f:
+            data = json.load(f)
+            for city_data in data["cities"]:
+                name = city_data["name"]
+                self.add_city(name)
+            for railway_data in data["railways"]:
+                city1 = self.get_city(railway_data["city1"])
+                city2 = self.get_city(railway_data["city2"])
+                railway = Railway(city1, city2, railway_data["length"], railway_data["color"])
+                city1.add_railway(railway)
+                city2.add_railway(railway)
+
+    def add_city(self, name):
+        city = City(name)
+        self.board[name] = city
+
+    def get_city(self, name):
+        return self.board[name]
+
+
+if __name__ == '__main__':
+    board = Board("data/board.json")
+    print(board)
